ec07301d493d69e8c5fa1d3c62405a92093c3571
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-28 20:37:25 +0800

    Added some helper functions

--- test.ts
+++ test.ts
@@ -17,9 +17,21 @@ function tryAccessToken() {
   console.log(todo.getTokenFromAuthorizationCode(['Tasks.Read', 'offline_access'], 'http://localhost', 'M6343fa4d-a72b-5bc3-9429-95d299fb99ff', '6ncgnkhy6w9'));
 }
 
+function tryAccessTokenFromRefreshToken() {
+  const userProperties = PropertiesService.getUserProperties();
+  const todo = new TODOOAuth2Client(userProperties, '3681c137-1a75-48cb-8628-fe61ed13dda3', userProperties.getProperty('client_secret'));
+  // eslint-disable-next-line no-console
+  console.log(todo.getTokenFromRefreshToken(['Tasks.Read', 'offline_access'], 'http://localhost', 'M6343fa4d-a72b-5bc3-9429-95d299fb99ff', userProperties.getProperty('refresh_token')));
+}
+
 function storeClientSecret() {
   const userProperties = PropertiesService.getUserProperties();
   userProperties.setProperty('client_secret', '');
+}
+
+function storeRefreshToken() {
+  const userProperties = PropertiesService.getUserProperties();
+  userProperties.setProperty('refresh_token', '');
 }
 
 function doGet(e: GoogleAppsScript.Events.DoGet) {
