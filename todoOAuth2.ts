5386c9cf05ddf4fdb12c091ed9d71f339340102a
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-28 18:00:48 +0800

    Added token endpoint

--- todoOAuth2.ts
+++ todoOAuth2.ts
@@ -3,6 +3,7 @@ import OAuth2Client from './oauth2';
 class TODOOAuth2Client extends OAuth2Client {
   static readonly WELL_KNOWN_URLS: Map<string, string> = new Map(<[string, string][]>([
     ['authorization_endpoint', 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'],
+    ['token_endpoint', 'https://login.microsoftonline.com/common/oauth2/v2.0/token'],
   ]))
 
   constructor(storage: GoogleAppsScript.Properties.Properties, clientID: string) {
