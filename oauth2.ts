07a4d6f6e543625856ecb675dbe4a953e548bf75
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-28 14:59:06 +0800

    Removed Storage

--- oauth2.ts
+++ oauth2.ts
@@ -1,13 +1,10 @@
-import PropertyStorage from './storage';
-
-
 class OAuth2Client {
   static readonly OAUTH2_STATE: string = "oauth2_state"
-  readonly storage: PropertyStorage
+  readonly storage:  GoogleAppsScript.Properties.Properties
   readonly wellKnownUrls: Map<String, String>
   readonly clientID: String
 
-  constructor(storage:PropertyStorage,clientID: String,wellKnownUrls: Map<String, String>) {
+  constructor(storage: GoogleAppsScript.Properties.Properties,clientID: String,wellKnownUrls: Map<String, String>) {
     this.wellKnownUrls = wellKnownUrls;
     this.storage = storage;
     this.clientID = clientID;
@@ -15,13 +12,13 @@ class OAuth2Client {
 
   authorize(redirectURI: String): GoogleAppsScript.HTML.HtmlOutput {
     const state = Math.random().toString(36).substring(2);
-    this.storage.set(OAuth2Client.OAUTH2_STATE, state);
+    this.storage.setProperty(OAuth2Client.OAUTH2_STATE, state);
     const authUrl = this.wellKnownUrls.get('authorization_endpoint');
     return OAuth2Client.redirect(`${authUrl}?client_id=${this.clientID}&state=${state}&redirect_uri=${redirectURI}`);
   }
 
   getToken(code: String, state: String) {
-    const storedState = this.storage.get(OAuth2Client.OAUTH2_STATE);
+    const storedState = this.storage.getProperty(OAuth2Client.OAUTH2_STATE);
     if (state !== storedState) {
       // eslint-disable-next-line no-undef
       throw new Error('invalid state');
