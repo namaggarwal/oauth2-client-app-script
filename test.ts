e7a399bcb409ec4b543acc928528339d742a3251
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-28 16:04:21 +0800

    Added doGet

--- test.ts
+++ test.ts
@@ -1,10 +1,14 @@
+/* eslint-disable no-unused-vars */
 import TODOOAuth2Client from './todoOAuth2';
 
-// eslint-disable-next-line no-unused-vars
 function tryIt() {
-  // eslint-disable-next-line no-undef
   const userProperties = PropertiesService.getUserProperties();
 
   const todo = new TODOOAuth2Client(userProperties, 'ssdsd');
   return todo.authorize('https://www.google.com');
+}
+
+
+function doGet(e: GoogleAppsScript.Events.DoGet) {
+  return tryIt();
 }
