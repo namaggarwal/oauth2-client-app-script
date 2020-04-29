92861323f2a277b8c354fde7441fbd1d156d6391
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-29 13:17:51 +0800

    Fixed tests

--- test.ts
+++ test.ts
@@ -6,22 +6,22 @@ function tryIt() {
   const userProperties = PropertiesService.getUserProperties();
 
   const todo = new TODOOAuth2Client(userProperties, '3681c137-1a75-48cb-8628-fe61ed13dda3', '');
-  return todo.authorize('http://localhost', ['tasks.readwrite', 'offline_access'], 'code');
+  return todo.authorize('http://localhost', ['https://outlook.office.com/tasks.readwrite', 'offline_access'], 'code');
 }
 
 
 function tryAccessToken() {
   const userProperties = PropertiesService.getUserProperties();
-  const todo = new TODOOAuth2Client(userProperties, '3681c137-1a75-48cb-8628-fe61ed13dda3', '');
+  const todo = new TODOOAuth2Client(userProperties, '3681c137-1a75-48cb-8628-fe61ed13dda3', userProperties.getProperty('client_secret'));
   // eslint-disable-next-line no-console
-  console.log(todo.getTokenFromAuthorizationCode(['tasks.readwrite', 'offline_access'], 'http://localhost', 'M6343fa4d-a72b-5bc3-9429-95d299fb99ff', '6ncgnkhy6w9'));
+  console.log(todo.getTokenFromAuthorizationCode(['https://outlook.office.com/tasks.readwrite', 'offline_access'], 'http://localhost', 'state', 'M6343fa4d-a72b-5bc3-9429-95d299fb99ff'));
 }
 
 function tryAccessTokenFromRefreshToken() {
   const userProperties = PropertiesService.getUserProperties();
   const todo = new TODOOAuth2Client(userProperties, '3681c137-1a75-48cb-8628-fe61ed13dda3', userProperties.getProperty('client_secret'));
   // eslint-disable-next-line no-console
-  console.log(todo.getTokenFromRefreshToken(['tasks.readwrite', 'offline_access'], 'http://localhost', userProperties.getProperty('refresh_token')));
+  console.log(todo.getTokenFromRefreshToken(['https://outlook.office.com/tasks.readwrite', 'offline_access'], 'http://localhost', userProperties.getProperty('refresh_token')));
 }
 
 function storeClientSecret() {
